e73b5dad4ec7e2863c67ceb177005ffbe7559705
Dan Thomas <dan.thomas@dc.gov>
2019-08-28 09:15:06 -0400

    Whitelist ip address to fix web console error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.web_console.whitelisted_ips = '172.22.0.1'
+
+
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
