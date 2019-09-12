b0639ddb720d34b09b3ccaf84a54032ecfb05d2c
harshaellanki <h_vardhan@yahoo.com>
2019-09-12 16:56:53 -0500

    WIP: added basic UI for starter pages

--- clients/html/src/app/starter/starter.component.ts
+++ clients/html/src/app/starter/starter.component.ts
@@ -1,13 +1,34 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   templateUrl: './starter.component.html',
-  styleUrls: ['./starter.component.css']
+  styleUrls: ['./starter.component.scss']
 })
-export class StarterComponent implements AfterViewInit {
+export class StarterComponent implements OnInit {
   subtitle: string;
+  currentTab: number = 0;
+  showPrevBtn: boolean = false;
+  showNextBtn: boolean =  true;
+
   constructor() {
     this.subtitle = 'This is some text within a card block.';
   }
 
-  ngAfterViewInit() {}
+  ngOnInit() {
+    this.showTab(0);
+  }
+
+  showTab(n) {
+    // ... and fix the Previous/Next buttons:
+    this.currentTab = n;
+    if (n === 0) {
+      this.showPrevBtn = false;
+    } else {
+      this.showPrevBtn = true;
+    }
+    if (n === 2) {
+      this.showNextBtn = false;
+    } else {
+      this.showNextBtn = true;
+    }
+  }
 }
