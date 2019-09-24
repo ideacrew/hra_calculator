947b21d664fe3955b1e2d02df03213483a8b0f08
saikumar9 <saikumarkotagiri9@gmail.com>
2019-09-24 17:20:07 -0400

    Start Months, End Months and some specs

--- app/models/operations/hra_default_setter.rb
+++ app/models/operations/hra_default_setter.rb
@@ -5,19 +5,36 @@ module Operations
 
     def call
       counties = ::Locations::CountyZip.all.pluck(:county_name).uniq
-      year = ::Date.today.year
+      # TODO: Change this in year 2020 to ::Date.today.year
+      year = ::Date.today.year + 1
       hra_defaulter = ::HraDefaulter.new({
         state_name: state_full_name,
         counties: counties,
         display_county: validate_county,
         display_zipcode: offerings_constrained_to_zip_codes,
-        start_month_dates: (Date.new(year).beginning_of_year..Date.new(year).end_of_year).map(&:to_s),
-        end_month_dates: (Date.new(year).beginning_of_year..Date.new(year).end_of_year.prev_month).map(&:to_s),
+        start_month_dates: fetch_start_dates(year),
+        end_month_dates: fetch_end_dates(year),
         tax_credit: tax_credit,
         market_place: market_place
       })
 
       Success(hra_defaulter)
+    end
+
+    def fetch_start_dates(year)
+      (1..12).inject([]) do |dates, mon|
+        dates << "#{year}-#{mon}-1"
+      end
+    end
+
+    def fetch_end_dates(year)
+      result_arr = (year..(year+1)).inject([]) do |dates, c_year|
+                     (1..12).each do |mon|
+                       dates << "#{c_year}-#{mon}-1"
+                     end
+                     dates
+                   end
+      result_arr - ["#{year + 1}-12-1"]
     end
   end
 end
