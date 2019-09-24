a5999220df1708240fb2709c2a31bf72baf54026
Dan Thomas <dan@ideacrew.com>
2019-09-23 23:05:22 -0400

    Fix Enterprise & Tenant entity names and attributes

--- app/entities/enterprise.rb
+++ app/entities/enterprise.rb
@@ -2,6 +2,7 @@ class Enterprise < Dry::Struct
   include DryStructSetters
   transform_keys(&:to_sym)
 
-  attribute :owner_organization_name, Types::String
   attribute :owner_account_id,        Types::String
+  attribute :owner_organization_name, Types::String.meta(omittable: true) 
+  attribute :tenants,                 Types::Array.of(Tenant).meta(omittable: true) 
 end
--- app/entities/tenant.rb
+++ app/entities/tenant.rb
@@ -3,7 +3,7 @@ class Tenant < Dry::Struct
   transform_keys(&:to_sym)
 
   attribute :key,               Types::Symbol
-  attribute :alias,             Types::String # => "Marketplace"
   attribute :name,              Types::String
+  attribute :alias,             Types::String.meta(omittable: true)  # => "Marketplace"
   attribute :owner_account_ids, Types::Array.of(Types::String)
 end
