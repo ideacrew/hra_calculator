40e12f3746d33248eb784506f36fdacbe3d5e2f9
Raghuram Ganjala <raghuramg83@gmail.com>
2019-09-27 08:39:45 -0400

    add update tenant service

--- app/controllers/admin/enterprise_controller.rb
+++ app/controllers/admin/enterprise_controller.rb
@@ -1,4 +1,33 @@
 class Admin::EnterpriseController < ApplicationController
+  
   def show
+  end
+  
+  def account_create
+  end
+
+  def tenant_create
+    result = Transactions::CreateTenant.new.call(tenant_params)
+
+    redirect_to :show
+  end
+  
+  def tenant_update
+    result = Transactions::UpdateTenant.new.call(tenant_params)
+
+    redirect_to :show
+  end
+  
+  def benefit_year_create
+  end
+  
+  def benefit_year_update
+  end
+
+  private
+
+  # filter tenant params
+  def tenant_params
+    params
   end
 end
--- app/models/transactions/update_tenant.rb
+++ app/models/transactions/update_tenant.rb
@@ -4,8 +4,6 @@ module Transactions
     include Dry::Transaction(container: ::Registry)
 
     step :fetch
-    step :construct_attributes
-    step :validate, with: "resource_registry.tenants.validate"
     step :persist
 
     private
@@ -14,22 +12,14 @@ module Transactions
       @tenant = Tenants::Tenant.find(tenant_id)
 
       if @tenant.blank?
-        Failure({errors: {enterprise_id: "Unabled to find tenant record with id #{tenant_id}"}})
+        Failure({errors: {tenant_id: "Unabled to find tenant record with id #{tenant_id}"}})
       else
         Success(input)
       end
     end
 
-    def construct_attributes(input)
-      tenant_params = @tenant.to_h.merge(input.to_h)
-      
-      Success(tenant_params)
-    end
-
-    def persist(input)
-      tenant = @tenant.assign_attributes(input.to_h)
-      
-      if tenant.save
+    def persist(input)      
+      if @tenant.update_attributes(input.to_h)
         Success(tenant)
       else
         Failure(tenant)
