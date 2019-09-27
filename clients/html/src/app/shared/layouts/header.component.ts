45a4164596edf19c288f067e88e01ad482a0f5bd
harshaellanki <h_vardhan@yahoo.com>
2019-09-27 11:16:26 -0400

    getting header footer info from config

--- clients/html/src/app/shared/layouts/header.component.ts
+++ clients/html/src/app/shared/layouts/header.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from './../../../environments/environment';
 
 @Component({
   selector: 'layout-header',
   templateUrl: './header.component.html',
   styleUrls: ['./../../app.component.scss']
 })
-export class HeaderComponent {
-  constructor() {}
+export class HeaderComponent implements OnInit {
+  tenant_logo_file: string;
+  tenant_url: string;
+  constructor(private httpClient: HttpClient,) { }
+
+  ngOnInit() {
+    this.getInitialInfo();
+  }
+
+  getInitialInfo() {
+    this.httpClient.get<any>(environment.apiUrl+"/hra_results/header_footer_config").subscribe(
+      (res) => {
+        console.log(res)
+        debugger
+        this.tenant_logo_file = res.data.tenant_logo_file;
+        this.tenant_url = res.data.tenant_url;
+      },
+      (err) => {
+        console.log(err)
+      }
+    );
+  }
 
 }
