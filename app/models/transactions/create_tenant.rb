6545c00c42d001ec058db3d7e15d750fb32c6ca2
Raghuram Ganjala <raghuramg83@gmail.com>
2019-09-27 07:55:56 -0400

    Updated tenant create/update transactions

--- app/models/transactions/create_tenant.rb
+++ app/models/transactions/create_tenant.rb
@@ -3,14 +3,26 @@ module Transactions
 
     include Dry::Transaction(container: ::Registry)
 
+    step :fetch
     step :construct_attributes
     step :validate, with: "resource_registry.tenants.validate"
     step :persist
 
     private
 
+    def fetch(input, enterprise_id:)
+      @enterprise = Enterprises::Enterprise.find(enterprise_id)
+      
+      if @enterprise.blank?
+        Failure({errors: {enterprise_id: "Unabled to find enterprise record with id #{enterprise_id}"}})
+      else
+        Success(input)
+      end
+    end
+
     def construct_attributes(input)
-      tenant_attributes = tenant_configurations.merge(input)
+      tenant_settings   = ResourceRegistry::AppSettings[:tenants][0]
+      tenant_attributes = tenant_settings.merge(input)
       
       Success(tenant_attributes)
     end
@@ -25,19 +37,15 @@ module Transactions
       end
     end
 
-    def persist(input, enterprise_id:)
+    def persist(input)
       tenant = Tenants::Tenant.new(input.to_h)
-      tenant.enterprise_id = enterprise_id
+      tenant.enterprise_id = @enterprise.id
       
       if tenant.save
         Success(tenant)
       else
         Failure(tenant)
       end
-    end
-
-    def tenant_configurations
-      ResourceRegistry::AppSettings[:tenants].first
     end
   end
 end
--- app/models/transactions/update_tenant.rb
+++ app/models/transactions/update_tenant.rb
@@ -1,33 +1,33 @@
 module Transactions
-  class CreateTenant
+  class UpdateTenant
 
     include Dry::Transaction(container: ::Registry)
 
-    step :find_tenant
+    step :fetch
     step :construct_attributes
     step :validate, with: "resource_registry.tenants.validate"
     step :persist
 
     private
 
-    def find_tenant(input)
-      tenant = Tenants::Tenant.find(input[:tenant_id])
-      
-      if tenant.blank?
-        Failure(input)
+    def fetch(input, tenant_id:)
+      @tenant = Tenants::Tenant.find(tenant_id)
+
+      if @tenant.blank?
+        Failure({errors: {enterprise_id: "Unabled to find tenant record with id #{tenant_id}"}})
       else
         Success(input)
       end
     end
 
     def construct_attributes(input)
-      tenant_params = tenant.to_h.merge(input.to_h)
+      tenant_params = @tenant.to_h.merge(input.to_h)
       
       Success(tenant_params)
     end
 
     def persist(input)
-      tenant = tenant.assign_attributes(input.to_h)
+      tenant = @tenant.assign_attributes(input.to_h)
       
       if tenant.save
         Success(tenant)
