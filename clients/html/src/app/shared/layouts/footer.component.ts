068d78a377e8a6429c979f257f5a7b34e6c2f631
harshaellanki <h_vardhan@yahoo.com>
2019-09-27 11:16:26 -0400

    getting header footer info from config

--- clients/html/src/app/shared/layouts/footer.component.ts
+++ clients/html/src/app/shared/layouts/footer.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from './../../../environments/environment';
 
 @Component({
   selector: 'layout-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./../../app.component.scss']
 })
-export class FooterComponent {
-  today: number = Date.now();
+export class FooterComponent implements OnInit{
+	customer_support_number: String;
+	benefit_year: String;
+	today: number = Date.now();
+
+	constructor(private httpClient: HttpClient,) { }
+
+
+  ngOnInit() {
+    this.getInitialInfo();
+  }
+
+  getInitialInfo() {
+    this.httpClient.get<any>(environment.apiUrl+"/hra_results/header_footer_config").subscribe(
+      (res) => {
+        console.log(res)
+        debugger
+        this.customer_support_number = res.data.customer_support_number;
+        this.benefit_year = res.data.benefit_year;
+      },
+      (err) => {
+        console.log(err)
+      }
+    );
+  }
 }
