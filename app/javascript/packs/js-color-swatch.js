16809ef0f43daa5d4481fe0e7a1295b1feab891a
Bill Transue <transue@gmail.com>
2019-10-01 09:49:26 -0400

    Make color inputs invalid/valid with JavaScript

--- app/javascript/packs/js-color-swatch.js
+++ app/javascript/packs/js-color-swatch.js
@@ -4,7 +4,9 @@ const colorPattern = /^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/
 for (const selectElement of selectElements) {
   selectElement.addEventListener('change', (e) => {
     if (e.currentTarget.value.match(colorPattern)) {
+      e.currentTarget.setCustomValidity("")
       e.currentTarget.classList.remove('is-invalid')
+      e.currentTarget.invalid = false
 
       var feedback = e.currentTarget.parentNode.querySelector('.invalid-feedback')
       if (feedback)
@@ -14,6 +16,7 @@ for (const selectElement of selectElements) {
       e.currentTarget.parentNode.querySelector('button').setAttribute("style", `background-color: ${color}`)
     } else {
       if (!e.currentTarget.classList.contains('is-invalid')) {
+        e.currentTarget.setCustomValidity("Invalid RGB value.")
         e.currentTarget.classList.add('is-invalid')
         var feedback = document.createElement('div')
         feedback.classList.add('invalid-feedback')
