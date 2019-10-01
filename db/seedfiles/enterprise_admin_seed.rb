1e07b352040253b9b21214d13d42e7d103a44b75
Raghuram Ganjala <raghuramg83@gmail.com>
2019-09-30 23:53:57 -0400

    Added benefit year settings to enterprise.yml and updated seeds to create default benefit years

--- db/seedfiles/enterprise_admin_seed.rb
+++ db/seedfiles/enterprise_admin_seed.rb
@@ -1,5 +1,6 @@
 puts "::: Cleaning Database :::"
 Enterprises::Enterprise.delete_all
+Enterprises::BenefitYear.delete_all
 Tenants::Tenant.delete_all
 Account.delete_all
 
@@ -8,8 +9,17 @@ enterprise = Enterprises::Enterprise.new(owner_organization_name: 'OpenHBX')
 
 enterprise_option = Options::Option.new(key: 'owner_organization_name', default: 'My Organization', description: 'Name of the organization that manages', type: 'string')
 enterprise.options = [enterprise_option]
-
 enterprise.save!
+
+ResourceRegistry::AppSettings[:options].each do |option_hash|
+  if option_hash[:key] == :benefit_years
+    option = Options::Option.new(option_hash)
+
+    option.child_options.each do |setting|
+      enterprise.benefit_years.create({ expected_contribution: setting.default, calendar_year: setting.key.to_s.to_i })
+    end
+  end
+end
 
 owner_account = Account.new(email: 'admin@openhbx.org', role: 'Enterprise Owner', uid: 'admin@openhbx.org', password: 'ChangeMe!', enterprise_id: enterprise.id)
 owner_account.save!
