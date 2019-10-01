06066dccaf9457ab0210c62e4ba51b30401e54f8
Raghuram Ganjala <raghuramg83@gmail.com>
2019-10-01 11:14:35 -0400

    Fixed products query and lowest cost plan costs

--- app/models/products/operations/fetch_products.rb
+++ app/models/products/operations/fetch_products.rb
@@ -4,18 +4,35 @@ module Products::Operations
 
     def call(hra_object)
       # age = ::Operations::AgeLookup.new.call(hra_object.age).success
-      products = ::Products::Product.where(
-        :premium_tables.exists => true,
-        :"premium_tables.premium_tuples".exists => true,
-        :"service_area_id".in => hra_object.service_area_ids,
-        :"premium_tables.rating_area_id" => BSON::ObjectId.from_string(hra_object.rating_area_id),
-        :"premium_tables.effective_period.min".lte => hra_object.start_month,
-        :"premium_tables.effective_period.max".gte => hra_object.start_month)
 
-      return Success(products) if products.present?
+      tenant = Tenants::Tenant.find_by_key(hra_object.tenant)
 
-      hra_object.errors += ['Could Not find any Products for the given data']
-      Failure(hra_object)
+      query_criteria = {
+          :premium_tables.exists => true,
+          :"premium_tables.premium_tuples".exists => true,
+          :"premium_tables.effective_period.min".lte => hra_object.start_month,
+          :"premium_tables.effective_period.max".gte => hra_object.start_month
+      }
+
+      if tenant.geographic_rating_area_model == 'zipcode'
+        query_criteria.merge!({
+          :"service_area_id".in => hra_object.service_area_ids,
+          :"premium_tables.rating_area_id" => BSON::ObjectId.from_string(hra_object.rating_area_id)
+          })
+      end
+
+      if tenant.geographic_rating_area_model == 'county'
+        query_criteria.merge!({:"service_area_id".in => hra_object.service_area_ids})
+      end
+
+      products = tenant.products.where(query_criteria)
+     
+      if products.present?
+        Success(products)
+      else
+        hra_object.errors += ['Could Not find any Products for the given data']
+        Failure(hra_object)
+      end
     end
   end
 end
