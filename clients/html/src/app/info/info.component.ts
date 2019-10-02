49ad26f23474b9716d7c9099a597db6d6855a547
harshaellanki <h_vardhan@yahoo.com>
2019-10-02 04:20:18 -0400

    passing dob params if dob field is enabled

--- clients/html/src/app/info/info.component.ts
+++ clients/html/src/app/info/info.component.ts
@@ -227,7 +227,9 @@ export class InfoComponent implements OnInit {
   onSubmit() {
     if (this.hraForm.valid) {
       var params = this.hraForm.value
-      params.dob = `${params.dob.year}-${params.dob.month}-${params.dob.day}`
+      if(this.showDob){
+        params.dob = `${params.dob.year}-${params.dob.month}-${params.dob.day}`
+      }
       this.resultService.setFormData(params);      
       this.httpClient.post<any>(environment.apiUrl+"/api/hra_results/hra_payload?tenant="+this.hostKey, this.hraForm.value).subscribe(
         (res) => {
