3442eb544ec13c4898e9a487559fb6f18086a934
Bill Transue <transue@gmail.com>
2019-10-02 09:13:26 -0400

    Fix DOB display in About Me Component

--- clients/html/src/app/app.component.ts
+++ clients/html/src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 import {NgxMaskModule} from 'ngx-mask'
+import { NgbDatepickerConfig, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateStandardParserFormatter } from "./ngb-date-standard-parser-formatter"
 
 @Component({
+  providers: [{provide: NgbDateParserFormatter, useClass: NgbDateStandardParserFormatter}],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
