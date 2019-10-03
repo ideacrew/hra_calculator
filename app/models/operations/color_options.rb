73b3964918f0665fb2a79680707db66eadaf4ef0
Battula <vrbattula@outlook.com>
2019-10-03 14:14:33 -0400

    Pass typeface options to the client side.

--- app/models/operations/color_options.rb
+++ app/models/operations/color_options.rb
@@ -6,10 +6,13 @@ module Operations
       site   = tenant.sites.first      
 
       ui_theme_option  = site.options.by_key(:ui_theme).first
+      ui_theme_options = ui_theme_option.options.pluck(:key, :value, :default)
+
       bootstrap_pallette_option  = ui_theme_option.options.by_key(:bootstrap_pallette).first
       color_options = bootstrap_pallette_option.options.pluck(:key, :value, :default)
+      options_hash = option_array_to_hash(color_options + ui_theme_options)
 
-      Success(option_array_to_hash(color_options))
+      Success(options_hash)
     end
 
     def option_array_to_hash(options)
