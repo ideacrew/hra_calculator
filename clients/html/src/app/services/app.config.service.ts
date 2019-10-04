956d83704fddb6caefee1982518ed03b162c7ae8
Bill Transue <transue@gmail.com>
2019-10-04 14:47:54 -0400

    WIP ConfigService to fetch site configs

---
+++ clients/html/src/app/services/app.config.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({providedIn: 'root'})
+export class ConfigService {
+  hostKey: string;
+
+constructor(private httpClient: HttpClient) {
+    if (environment.production) {
+      this.hostKey = window.location.host.split(".",1)[0];
+    } else {
+      this.hostKey = "dc";
+    }
+  }
+}
