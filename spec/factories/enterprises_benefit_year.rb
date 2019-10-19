9e18974bf22e97fcc73a8e978cc0085222b94755
saikumar9 <saikumarkotagiri9@gmail.com>
2019-10-19 15:08:39 -0400

    Factories changes

--- spec/factories/enterprises_benefit_year.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :benefit_year, class: '::Enterprises::BenefitYear' do
-    expected_contribution { 0.976 }
-    calendar_year         { 2020 }
-    description           { 'BenefitYear for 2020' }
-  end
-end
