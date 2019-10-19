8ab3d264e88d2dd22131c0a7c447dce4ab926431
saikumar9 <saikumarkotagiri9@gmail.com>
2019-10-19 15:08:39 -0400

    Factories changes

--- spec/factories/options_option.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :option, class: '::Options::Option' do
-    key         {'owner_organization_name'}
-    default     {'My Organization'}
-    description {'Name of the organization that manages'}
-    type        {'string'}
-  end
-end
