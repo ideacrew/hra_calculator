572f50709ff4c7d3ef4fbb8c2c8c17b78f92101c
saikumar9 <saikumarkotagiri9@gmail.com>
2019-10-21 15:05:13 -0400

    Bring back start and end dates

--- spec/controllers/api/configurations_controller_spec.rb
+++ spec/controllers/api/configurations_controller_spec.rb
@@ -47,6 +47,7 @@ RSpec.describe Api::ConfigurationsController, dbclean: :after_each do
         it 'should have all the keys for the data' do
           attr_keys = @result['data'].keys
           HraDefaulter.schema.keys.map(&:name).map(&:to_s).each do |keyy|
+            next keyy if ["start_month_dates", "end_month_dates"].include?(keyy)
             expect(attr_keys.include?(keyy)).to be_truthy
           end
         end
@@ -135,6 +136,7 @@ RSpec.describe Api::ConfigurationsController, dbclean: :after_each do
         it 'should have all the keys for the data' do
           attr_keys = @result['data'].keys
           HraDefaulter.schema.keys.map(&:name).map(&:to_s).each do |keyy|
+            next keyy if ["start_month_dates", "end_month_dates"].include?(keyy)
             expect(attr_keys.include?(keyy)).to be_truthy
           end
         end
@@ -259,6 +261,7 @@ RSpec.describe Api::ConfigurationsController, dbclean: :after_each do
         it 'should have all the keys for the data' do
           attr_keys = @result['data'].keys
           HraDefaulter.schema.keys.map(&:name).map(&:to_s).each do |keyy|
+            next keyy if ["start_month_dates", "end_month_dates"].include?(keyy)
             expect(attr_keys.include?(keyy)).to be_truthy
           end
         end
