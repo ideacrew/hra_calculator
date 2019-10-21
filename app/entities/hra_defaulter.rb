6f4ced24b48b8bdc573805c4e30586948d9452db
saikumar9 <saikumarkotagiri9@gmail.com>
2019-10-21 15:01:07 -0400

    Merge conflict

--- app/entities/hra_defaulter.rb
+++ app/entities/hra_defaulter.rb
@@ -2,13 +2,13 @@ class HraDefaulter < Dry::Struct
   include DryStructSetters
   transform_keys(&:to_sym)
 
-  attribute :state_name,        Types::String
-  attribute :counties,          Types::Array.of(Types::String)
-  attribute :display_county,    Types::Bool
-  attribute :display_zipcode,   Types::Bool
-  attribute :tax_credit,        Types::String
-  attribute :market_place,      Types::String
-  attribute :ui_pages,          Types::Array
-  attribute :colors,            Types::Array
-  attribute :features,          Types::Array
+  attribute :state_name, Types::String
+  attribute :counties, Types::Array.of(Types::String)
+  attribute :display_county, Types::Bool
+  attribute :display_zipcode, Types::Bool
+  attribute :tax_credit, Types::String
+  attribute :market_place, Types::String
+  attribute :ui_pages, Types::Array
+  attribute :colors,   Types::Array
+  attribute :features,   Types::Array
 end
