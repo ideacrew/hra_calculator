090b945b6da868fd35a919bdcc9b0b66b37d4948
Mark Goho <markgoho@gmail.com>
2019-10-29 13:33:46 -0400

    move css to component scss files

--- clients/html/src/app/shared/layouts/footer.component.ts
+++ clients/html/src/app/shared/layouts/footer.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { HeaderFooterConfigurationProvider, HeaderFooterConfigurationService } from "../../configuration/header_footer/header_footer_configuration.service";
-import { HeaderFooterConfigurationResource } from "../../configuration/header_footer/header_footer_configuration.resources";
+import {
+  HeaderFooterConfigurationProvider,
+  HeaderFooterConfigurationService
+} from '../../configuration/header_footer/header_footer_configuration.service';
+import { HeaderFooterConfigurationResource } from '../../configuration/header_footer/header_footer_configuration.resources';
 
 @Component({
   selector: 'layout-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./../../app.component.scss']
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit{
-	customer_support_number: String;
-	benefit_year: number;
-	today: number = Date.now();
+export class FooterComponent implements OnInit {
+  customer_support_number: String;
+  benefit_year: number;
+  today: number = Date.now();
   primaryColorCode: string;
 
-	constructor(
-    @Inject(HeaderFooterConfigurationService.PROVIDER_TOKEN) private configurationProvider: HeaderFooterConfigurationProvider,
+  constructor(
+    @Inject(HeaderFooterConfigurationService.PROVIDER_TOKEN)
+    private configurationProvider: HeaderFooterConfigurationProvider
   ) {
     this.benefit_year = new Date().getFullYear() + 1;
   }
-
 
   ngOnInit() {
     this.getInitialInfo();
@@ -28,7 +31,9 @@ export class FooterComponent implements OnInit{
     this.configurationProvider.getHeaderFooterConfiguration(this);
   }
 
-  applyHeaderFooterConfiguration(resource : HeaderFooterConfigurationResource) : void {
+  applyHeaderFooterConfiguration(
+    resource: HeaderFooterConfigurationResource
+  ): void {
     if (resource.call_center_phone != null) {
       this.customer_support_number = resource.call_center_phone;
     }
