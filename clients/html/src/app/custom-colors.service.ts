f200a954eb3d7765d9aff4ce6a59c5e4c903cce9
Mark Goho <markgoho@gmail.com>
2019-11-15 10:33:00 -0500

    add contrast ratio utility

--- clients/html/src/app/custom-colors.service.ts
+++ clients/html/src/app/custom-colors.service.ts
@@ -17,6 +17,11 @@ export class CustomColorsService {
       warning_color
     } = customColors;
 
+    const invertedTextColor =
+      this.getContrastRatio(primary_color, '#f1f1f1') < 4.5
+        ? '#010101'
+        : '#f1f1f1';
+
     const css = `
     :root {
       --danger-color: ${danger_color};
@@ -25,6 +30,7 @@ export class CustomColorsService {
       --secondary-color: ${secondary_color};
       --success-color: ${success_color};
       --warning-color: ${warning_color};
+      --inverted-text-color: ${invertedTextColor};
     }
     `;
 
@@ -38,26 +44,45 @@ export class CustomColorsService {
   }
 
   // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-  hexToRgb(hex: string) {
-    const [red, green, blue] = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
-      hex
-    );
-    return [parseInt(red, 16), parseInt(green, 16), parseInt(blue, 16)];
+  hexToRgb(hex: string): number[] {
+    return hex
+      .replace(
+        /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
+        (m, r, g, b) => '#' + r + r + g + g + b + b
+      )
+      .substring(1)
+      .match(/.{2}/g)
+      .map(x => parseInt(x, 16));
   }
 
-  getLuminance(rgbValue: number[]) {
-    // Formula: http://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
-    for (let value of rgbValue) {
-      value /= 255;
+  // Formula: http://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
+  getLuminance(rgbValues: number[]): number {
+    const [red, green, blue] = rgbValues.map(val => {
+      const srgb = val / 255;
 
-      value =
-        value < 0.03928
-          ? value / 12.92
-          : Math.pow((value + 0.055) / 1.055, 2.4);
+      return srgb <= 0.03928
+        ? srgb / 12.92
+        : Math.pow((srgb + 0.055) / 1.055, 2.4);
+    });
 
-      value[i] = value;
-    }
+    return 0.2126 * red + 0.7152 * green + 0.0722 * blue;
+  }
 
-    return 0.2126 * rgba[0] + 0.7152 * rgba[1] + 0.0722 * rgba[2];
+  // https://www.w3.org/TR/2008/REC-WCAG20-20081211/#contrast-ratiodef
+  getContrastRatio(hex1: string, hex2: string): number {
+    const rgb1 = this.hexToRgb(hex1);
+    const rgb2 = this.hexToRgb(hex2);
+
+    const luminance1 = this.getLuminance(rgb1) + 0.05;
+    const luminance2 = this.getLuminance(rgb2) + 0.05;
+
+    const contrastRatio =
+      luminance1 > luminance2
+        ? luminance1 / luminance2
+        : luminance2 / luminance1;
+
+    // Just grab last two decimals
+    // https://github.com/LeaVerou/contrast-ratio/blob/gh-pages/color.js#L167
+    return Math.floor(contrastRatio * 100) / 100;
   }
 }
