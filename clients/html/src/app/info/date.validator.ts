3a33d8f5676828ee6d74d3aacf334b4244b7298c
Mark Goho <markgoho@gmail.com>
2019-11-26 14:25:57 -0500

    remove unused components

--- clients/html/src/app/info/date.validator.ts
+++ clients/html/src/app/info/date.validator.ts
@@ -1,22 +1,35 @@
 import { FormControl } from '@angular/forms';
-import { max } from 'd3';
 
 export function validateDate(control: FormControl) {
-	if(control.value && control.value.year && control.value.month && control.value.day){
-	  let minDate = new Date("1900-01-01")
-	  let maxDate = new Date()
-		let givenDate = new Date(`${control.value.year}-${control.value.month}-${control.value.day}`)
-	  if (givenDate < minDate || givenDate > maxDate) {
-	    return {
-	      invalidDate: true
-	    };
-	  }
-	}
+  if (
+    control.value &&
+    control.value.year &&
+    control.value.month &&
+    control.value.day
+  ) {
+    const minDate = new Date('1900-01-01');
+    const maxDate = new Date();
+    const givenDate = new Date(
+      `${control.value.year}-${control.value.month}-${control.value.day}`
+    );
+    if (givenDate < minDate || givenDate > maxDate) {
+      return {
+        invalidDate: true
+      };
+    }
+  }
   return null;
 }
 
 export function validateDateFormat(control: FormControl) {
-	if(!(control.value && control.value.year && control.value.month && control.value.day)){
+  if (
+    !(
+      control.value &&
+      control.value.year &&
+      control.value.month &&
+      control.value.day
+    )
+  ) {
     return {
       invalidDateFormat: true
     };
